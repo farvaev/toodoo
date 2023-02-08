39d13b2700bb64d7fc592fa56804d9a74c011302
Farvaev Emil <farvaev.emil@gmail.com>
2023-02-09 02:30:17 +0300

    Updates

--- output/server/entries/pages/about/_page.svelte.js
+++
@@ -1,9 +0,0 @@
-import { c as create_ssr_component } from "../../../chunks/index.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<head><title>about - toodoo</title></head>
-
-<p class="${"text-lg underline"}">and this is about page</p>`;
-});
-export {
-  Page as default
-};
--- output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-export const component = async () => (await import('../entries/pages/about/_page.svelte.js')).default;
-export const file = '_app/immutable/components/pages/about/_page.svelte-58d3dc6f.js';
-export const imports = ["_app/immutable/components/pages/about/_page.svelte-58d3dc6f.js","_app/immutable/chunks/index-7ccf10d2.js"];
-export const stylesheets = [];
-export const fonts = [];
